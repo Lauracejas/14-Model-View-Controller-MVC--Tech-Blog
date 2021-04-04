d4558df87a0b240ba0d3e929414ccaad759e1916
lauracejas <lauracejasmartin94@gmail.com>
2021-04-04 13:02:27 -0400

    index.js controllers

--- controllers/index.js
+++ controllers/index.js
@@ -4,12 +4,12 @@ const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes.js');
 
 
-router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-    res.status(404).end();
-  });
+// router.use((req, res) => {
+//     res.status(404).end();
+//   });
 
 module.exports = router;
